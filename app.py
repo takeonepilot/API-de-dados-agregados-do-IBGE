7e107a5c962f6feccb46ec717508d515567ee462
takeonepilot <121069437+takeonepilot@users.noreply.github.com>
2023-02-17 10:08:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from controllers.municipios_controller import municipios_controller
+from municipios_controller import MunicipiosController
 
 app = Flask(__name__)
-app.register_blueprint(municipios_controller)
+municipios_controller = MunicipiosController()
+
+app.add_url_rule('/municipios', view_func=municipios_controller.get_all_municipios, methods=['GET'])
+app.add_url_rule('/municipios/<codigo>', view_func=municipios_controller.get_municipio_by_codigo, methods=['GET'])
 
 if __name__ == '__main__':
     app.run()
