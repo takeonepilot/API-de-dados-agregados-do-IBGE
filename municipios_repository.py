9626dda873b22b8effef1a09e003c5af2b78f115
takeonepilot <121069437+takeonepilot@users.noreply.github.com>
2023-02-17 10:09:38 -0300

    Update municipios_repository.py

--- municipios_repository.py
+++ municipios_repository.py
@@ -2,15 +2,4 @@ import requests
 
 class MunicipiosRepository:
     def __init__(self):
-        self.url = 'https://servicodados.ibge.gov.br/api/v1/localidades/municipios'
-
-    def listar_municipios(self):
-        response = requests.get(self.url)
-        municipios = response.json()
-        return municipios
-
-    def obter_municipio(self, codigo):
-        url_municipio = f'{self.url}/{codigo}'
-        response = requests.get(url_municipio)
-        municipio = response.json()
-        return municipio
+        self.base_url = 'https://servicodados.ibge.gov.br/api/v1/localidades/m
